b2b7262683ea4ed3702a222adc5622f00efa5b0a
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-09-17 12:29:43 -0300

    Solução 3/5

---
+++ Fundamentos Aritmeticos/3 - Análise de Números/solucao.js
@@ -0,0 +1,14 @@
+numero = Array(5);
+numero[0] = gets();
+numero[1] = gets();
+numero[2] = gets();
+numero[3] = gets();
+numero[4] = gets();
+positivos = numero.filter(value => value > 0);
+negativos = numero.filter(value => value < 0);
+pares = numero.filter(value => value % 2 == 0);
+impares = numero.filter(value => value % 2 != 0);
+console.log(pares.length + " valor(es) par(es)");
+console.log(impares.length + " valor(es) impar(es)");
+console.log(positivos.length + " valor(es) positivo(s)");
+console.log(negativos.length + " valor(es) negativo(s)");

7934b85720b4bc0c29065da68e4663c52c98479a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-21 14:42:15 -0300

    introducao 2 js

---
+++ Desafios/JavaScript/7. Introdução a Programação com JavaScript/2. Multiplicação Simples/solucao.js
@@ -0,0 +1,22 @@
+/**
+ * 2/3 - Multiplicação Simples
+ * 
+ * Desafio
+ *      Você receberá dois valores inteiros. Faça a leitura e em seguida calcule 
+ * o produto entre estes dois valores. Atribua esta operação à variável PROD, 
+ * mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
+ * 
+ * Entrada
+ *      A entrada contém 2 valores inteiros.
+ * 
+ * Saída
+ *      Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um 
+ * espaço em branco antes e depois da igualdade.
+ */
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+
+let total = valor1 * valor2;
+
+console.log("PROD = " + total);
---
+++ Desafios/JavaScript/7. Introdução a Programação com JavaScript/3. Folha de Pagamento/solucao.js
@@ -0,0 +1,30 @@
+/**
+ * 3/3 - Folha de Pagamento
+ * 
+ * Desafio
+ *      Precisamos saber quanto uma determinada empresa deve pagar para seus 
+ * colaboradores, porém temos apenas a quantidade de horas trabalhadas e o valor 
+ * hora. Escreva um programa que leia o número de um colaborador, seu número de 
+ * horas trabalhadas, o valor que recebe por hora e calcula o salário desse 
+ * colaborador. Em seguida, apresente o número e o salário do colaborador, com duas 
+ * casas decimais.
+ * 
+ * Entrada
+ *      Você receverá 2 números inteiros e 1 número com duas casas decimais, 
+ * representando o número, quantidade de horas trabalhadas e o valor que o 
+ * funcionário recebe por hora trabalhada.
+ * 
+ * Saída
+ *      Exiba o número e o salário do colaborador, conforme exemplo abaixo, com um 
+ * espaço em branco antes e depois da igualdade. No caso do salário, também deve 
+ * haver um espaço em branco após o $.
+ */
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+
+let salary = parseFloat(valor2 * valor3).toFixed(2);
+
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);

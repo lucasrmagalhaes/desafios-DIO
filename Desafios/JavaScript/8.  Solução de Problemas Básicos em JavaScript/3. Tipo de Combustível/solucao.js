b2cc10a07cedcb90889b3baa9a651bf6330a7366
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-02-07 17:45:18 -0300

    em andamento

--- Desafios/JavaScript/8.  Solução de Problemas Básicos em JavaScript/3. Tipo de Combustível/solucao.js
+++ Desafios/JavaScript/8.  Solução de Problemas Básicos em JavaScript/3. Tipo de Combustível/solucao.js
@@ -14,30 +14,3 @@
  de combustível, conforme exemplo.
 */
 
-let alcohol = 0;
-let gasoline = 0;
-let diesel = 0;
-
-while (     ) {
-  const code = Number(lines.shift());
-  
-  switch (code) {
-    case 1:
-      
-      break;
-    case 2:
-
-      break;
-    case 3:
-  
-      break;
-    case 4:
-      
-      break;
-  }
-}
-
-console.log("MUITO OBRIGADO");
-console.log("Alcool: " + alcohol);
-console.log("Gasolina: " + gasoline);
-console.log("Diesel: " + diesel);

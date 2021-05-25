64d9258e1a80c7794615ee662a817eb297ceb86c
saralyma <84419912+saralyma@users.noreply.github.com>
2021-05-24 21:29:55 -0300

    Duvidas
    Obrigado pela resposta do desafio , porem eu nao consigo enviar lá na DIO , ele fica processando e nao faz o teste , é assim mesmo ??? outra duvida , para o valor de X , voce vc colocou = parseint (line[0] ) ; mas eu deveria declarar o valor da variavel  não e ? porque ai eu nao entendi !!! pode me ajudar ??/ agradeço !
    obs eu mudei ....

--- Desafios/JavaScript/1. Introdução a Programação/1.1. Dvividindo X por Y/solucao.js
+++ Desafios/JavaScript/1. Introdução a Programação/1.1. Dvividindo X por Y/solucao.js
@@ -20,8 +20,8 @@ let limit = parseInt(gets());
 for (let i = 0; i < limit; i++) {
     
     let line = gets().split(" ");
-    let X = parseInt(line[0]);
-    let Y = parseInt(line[1]);
+    let X = parseInt(line[3]);
+    let Y = parseInt(line[2]);
     
     if (Y == 0) {
         console.log("divisao impossivel");
